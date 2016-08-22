52d6e29d88203cda2994590690e624d80d2c29eb
Kevin Whitman <keverw92@gmail.com>
2016-08-22 07:05:56 -0400

    Updated loading screen where it can be more dynamic based on the data the main thread will be sending

--- renderer.js
+++ renderer.js
@@ -5,13 +5,74 @@ var util = require('./modules/util.js');
 var irpc = require('electron-irpc');
 var irpcRenderer = irpc.renderer();
 
+var dbResultLastUIState = '';
+var isDBReady = function(isReadyCB)
+{
+    irpcRenderer.call('dbHelpers.isReady', {}, function(err, result)
+    {
+        if (err)
+        {
+            throw err;
+        }
+        else if (result.ready)
+        {
+            isReadyCB();
+        }
+        else
+        {
+            //update ui based on result
+            var dbResultUIState = [result.dbTask, result.totalSteps, result.doneSteps].join(',');
+
+            if (dbResultLastUIState != dbResultUIState)
+            {
+                dbResultLastUIState = dbResultUIState;
+
+                //ui data state changed, update h4
+                if (result.dbTask == 'i')
+                {
+                    $('#loadingScreen h4').text(global.lang.loadingScreen.init).show();
+                }
+                else if (result.dbTask == 'u')
+                {
+                    $('#loadingScreen h4').text(global.lang.loadingScreen.upgrade).show();
+                }
+
+                //update progress
+                if (result.totalSteps > 0)
+                {
+                    var precent = Math.floor(((result.doneSteps / result.totalSteps) * 100));
+                    $('#loadingScreen .progress-bar').attr('aria-valuenow', precent).css('width', precent + '%').text(precent + '%');
+                    $('#loadingScreen .progress').show();
+                }
+
+            }
+
+            //ask again in 500 millsseconds
+            setTimeout(function()
+            {
+                isDBReady(isReadyCB);
+            }, 500);
+
+        }
+
+    });
+};
+
 $(function()
 {
-    $('#appView').html(util.getViewHtml('base/main'));
-    $('#loadingScreen').fadeOut('fast', function()
+    $('#loadingScreen h1').text(global.lang.loadingScreen.loading).show();
+
+    isDBReady(function()
     {
-        $('#appView').fadeIn('fast');
-        $('#menuDropdownName').text('Accounts');
-        $('#menuDropdownItems').html(util.getViewHtml('base/menuDropdown'));
+        $('#appView').html(util.getViewHtml('base/main'));
+        $('#loadingScreen').fadeOut('fast', function()
+        {
+            $('#appView').fadeIn('fast');
+            $('#menuDropdownName').text('Accounts');
+            $('#menuDropdownItems').html(util.getViewHtml('base/menuDropdown'));
+        });
+
     });
+
 });
+
