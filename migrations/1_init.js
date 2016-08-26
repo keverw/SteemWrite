f0d642069b28e4a6e9837ddd94786ab8ca08c67f
Kevin Whitman <keverw92@gmail.com>
2016-08-26 09:03:50 -0400

    set the default web socket node

--- migrations/1_init.js
+++ migrations/1_init.js
@@ -4,9 +4,10 @@
 
     var cmds = {
         auto_vacuum: 'PRAGMA auto_vacuum = 1',
-        make_cvsTBL: 'CREATE TABLE `kvs` (`k` TEXT, `v`	TEXT, PRIMARY KEY(k));'
+        make_cvsTBL: 'CREATE TABLE `kvs` (`k` TEXT, `v`	TEXT, PRIMARY KEY(k));',
+        insertDefault_host: 'INSERT INTO kvs (`k`, `v`) VALUES ("defaultWS", "' + global.appConfig.defaultWS + '");'
     };
-
+    
     module.exports = {
         getTotal: Object.keys(cmds).length, //how many things you plan todo
         migrate: function(doneCB)
