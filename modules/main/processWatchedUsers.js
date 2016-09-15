d537df40ade8f8a6a414b9a17037ed984cc18d0d
Kevin Whitman <keverw92@gmail.com>
2016-09-15 04:53:37 -0400

    progress on importing posts

--- modules/main/processWatchedUsers.js
+++ modules/main/processWatchedUsers.js
@@ -2,7 +2,8 @@
 {
     var _ = require('underscore'),
         moment = require('moment-timezone'),
-        postHelpers = require('./postHelpers.js');
+        postHelpers = require('./postHelpers.js'),
+        util = require('../util.js');
 
     module.exports = {
         processItem: function(reqMeta, resultData, cb)
@@ -17,7 +18,7 @@
             var doneTasks = 0;
             var lastErr = null;
 
-            console.log(resultData);
+            //console.log(resultData);
 
             ////////////////////////////////////////////////////////////
             if (updaterMode)
@@ -79,8 +80,130 @@
             var reqID = reqMeta.modes;
             var username = reqMeta.username;
 
-            //todo: write this
-            cb();
+            ////////////////////////////
+            var trx_id = resultData.trx_id;
+            var timestamp = resultData.timestamp;
+
+            var op = resultData.op[0];
+            var data = resultData.op[1];
+
+            //console.log(op, data);
+
+            if (op == 'comment') //account for comments
+            {
+                //new post or user for that watched user
+                if (data.parent_author === '' && data.author === username)
+                {
+
+                    //check if author, permlink exists already
+                    global.db.get('SELECT * FROM posts WHERE author = ? AND permlink = ?', [data.author, data.permlink], function(err, row)
+                    {
+                        if (err) return cb(err);
+
+                        var metadata = JSON.parse(data.json_metadata);
+
+                        var tag1 = (metadata.tags && metadata.tags[0]) ? metadata.tags[0] : '';
+                        var tag2 = (metadata.tags && metadata.tags[1]) ? metadata.tags[1] : '';
+                        var tag3 = (metadata.tags && metadata.tags[2]) ? metadata.tags[2] : '';
+                        var tag4 = (metadata.tags && metadata.tags[3]) ? metadata.tags[3] : '';
+                        var tag5 = (metadata.tags && metadata.tags[4]) ? metadata.tags[4] : '';
+
+                        //grr... if already need to caulate body...
+                        //okay.... move this function to postHelpers.
+                        //var revHash = [data.author, data.permlink.data.title].join('.');
+                        //revHash should be: body, json_metadata
+
+                        var featuredImg = '';
+
+                        if (metadata.image && typeof metadata.image == 'object' && typeof metadata.image[0] == 'string')
+                        {
+                            featuredImg = metadata.image[0];
+                        }
+
+                        var unixTime = moment(timestamp).unix();
+
+                        if (row) //already
+                        {
+                            //grab revision before this one
+                            global.db.get('SELECT body FROM revisions WHERE author = ? AND permlink = ? AND blockChainDate > 0 AND blockChainDate < ? ORDER BY blockChainDate DESC LIMIT 1', [data.author, data.permlink, unixTime], function(err, row)
+                            {
+                                if (err) return cb(err);
+
+                                var body = data.body;
+
+                                if (row) //found a revision before
+                                {
+                                    //update body
+                                    body = util.applyPatch(row.body, data.body);
+                                }
+
+                                //insert revision
+                                var revHash = postHelpers.generateRevHash(data.author, data.permlink, data.title, body, JSON.stringify(metadata));
+
+                            });
+
+                            // CREATE TABLE `posts` (`author` TEXT, `permlink` TEXT, `title` TEXT, `status` TEXT, `latestPublishedTX` TEXT, `revHash` TEXT, `date` INTEGER, `scheduledDate` INTEGER, `tag1` TEXT, `tag2` TEXT, `tag3` TEXT, `tag4` TEXT, `tag5` TEXT, `featuredImg` TEXT, PRIMARY KEY(`author`,`permlink`));
+                            // CREATE TABLE `revisions` (`revHash` TEXT, `publishedTX` TEXT, `author` TEXT, `permlink` TEXT, `title` TEXT, `body` TEXT, `json_metadata` TEXT, `localDate` INTEGER, `blockChainDate` INTEGER, `date` INTEGER, `isAutosave` INTEGER, PRIMARY KEY(`revHash`));
+
+                            //insert revisions
+
+                            //update posts table if new revision is one newer than the last revision
+
+                        }
+                        else //not already
+                        {
+                            //insert revision
+                            var revHash = postHelpers.generateRevHash(data.author, data.permlink, data.title, data.body, JSON.stringify(metadata));
+
+                            global.db.run("INSERT OR IGNORE INTO revisions (`revHash`, `publishedTX`, `author`, `permlink`, `title`, `body`, `json_metadata`, `localDate`, `blockChainDate`, `date`, `isAutosave`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [revHash, trx_id, data.author, data.permlink, data.title, data.body, JSON.stringify(metadata), 0, unixTime, unixTime, 0], function(err)
+                            {
+                                if (err) return cb(err);
+
+                                //insert post
+                                global.db.run("INSERT OR IGNORE INTO posts (`author`, `permlink`, `title`, `status`, `latestPublishedTX`, `revHash`, `date`, `scheduledDate`, `tag1`, `tag2`, `tag3`, `tag4`, `tag5`, `featuredImg`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [data.author, data.permlink, data.title, 'published', trx_id, revHash, unixTime, 0, tag1, tag2, tag3, tag4, tag5, featuredImg], function(err)
+                                {
+                                    cb(err);
+                                });
+
+                            });
+
+                        }
+
+                    });
+
+                    //   [3, {
+                    //   "id": "2.17.7483135",
+                    //   "trx_id": "fe9aae14ef39eb99a8fb4be85997d721d682a895",
+                    //   "block": 4854475,
+                    //   "trx_in_block": 4,
+                    //   "op_in_trx": 0,
+                    //   "virtual_op": 24532,
+                    //   "timestamp": "2016-09-10T17:52:21",
+                    //   "op": ["comment", {
+                    //     "parent_author": "",
+                    //     "parent_permlink": "spam",
+                    //     "author": "kevintester",
+                    //     "permlink": "testing",
+                    //     "title": "testing",
+                    //     "body": "test\n\nPlease ignore this post",
+                    //     "json_metadata": "{\"tags\":[\"spam\"]}"
+                    //   }]
+                    // }]
+
+                }
+                else
+                {
+                    cb();
+                }
+
+            }
+            else
+            {
+                cb();
+            }
+
+            //todo: account for delete's
+
         }
     };
 
